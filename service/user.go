fa56d15cf1defddf5d7cb43815c4a2dadf53a94a
Wuchenglin1 <1336636739@qq.com>
2021-12-09 22:32:15 +0800

    新增了密码加盐处理

--- service/user.go
+++ service/user.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"golang.org/x/crypto/bcrypt"
 	"message-board-demo/dao"
 	"message-board-demo/model"
 )
@@ -15,6 +16,9 @@ func IsRepeatUsername(username string) (model.User, error) {
 }
 
 func Register(user model.User) error {
+	//在此处将密码进行加盐哈希处理
+	hash, _ := bcrypt.GenerateFromPassword([]byte(user.UserPassword), bcrypt.DefaultCost)
+	user.UserPassword = string(hash)
 	err := dao.InsertUser(user)
 	return err
 }
@@ -22,11 +26,13 @@ func Register(user model.User) error {
 // IsUserCorrect 检查账号密码是否正确，如果正确返回一个true，若错误则返回一个false
 func IsUserCorrect(user *model.User) bool {
 	iUser, _ := dao.SelectUsername(user.Username)
-	if user.UserPassword == iUser.UserPassword {
+	err := bcrypt.CompareHashAndPassword([]byte(iUser.UserPassword), []byte(user.UserPassword))
+	if err == nil {
 		user.Id = iUser.Id
 		return true
+	} else {
+		return false
 	}
-	return false
 }
 
 func AddMB(mb model.MiBao) error {
